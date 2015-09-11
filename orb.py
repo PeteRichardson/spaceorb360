177cbdb1206f4f919dfa86d6ce62b2b1f3a36a26
Pete Richardson <pete@peterichardson.com>
2015-09-10 22:09:26 -0700

    break out packet types into classes
    Now we can iterate on better dump for each packet type separately

--- orb.py
+++ orb.py
@@ -2,21 +2,65 @@
 
 import serial
 
+class Packet(object):
+    desc = ""
+    def __init__(self, bytes):
+        self.header = bytes[0]
+        #assert(self.header in orb_packet_types.keys())
+
+    @classmethod
+    def create(cls, bytes):
+        header = bytes[0]
+        if header == 'R':
+            result = ResetPacket(bytes)
+        elif header == 'D':
+            result =  BallDataPacket(bytes)            
+        elif header == 'K':
+            result = ButtonDataPacket(bytes)
+        elif header == 'E':
+            result = ErrorPacket(bytes)
+        elif header == 'N':
+            result = NullRegionPacket(bytes)
+        elif header == '!':
+            result = InformationPacket(bytes)
+        else:
+            result = UnknownPacket(bytes)
+        return result
+
+    def __str__(self):
+        return self.desc
+
+class ResetPacket(Packet):
+    desc = 'reset'
+
+class BallDataPacket(Packet):
+    desc = 'ball data'
+
+class ButtonDataPacket(Packet):
+    desc = 'button data'
+
+class ErrorPacket(Packet):
+    desc = 'error'
+
+class NullRegionPacket(Packet):
+    desc = 'null region'
+
+class InformationPacket(Packet):
+    desc = 'information'
+
+class UnknownPacket(Packet):
+    desc = 'unknown'
+
 ser = serial.Serial("/dev/cu.usbserial-AJ03ACPV", baudrate=9600, parity='N', stopbits=1)  
-print(ser)
-#ser.write("?\r")      # write a string
 try:
     line = []
     while True:
         for c in ser.read():
-            if ord(c) > 128:
-                c = chr(ord(c) - 128)
-                #line.append(c.encode('hex'))
-                line.append(c)
-            else:
-                line.append(c)
+                 #line.append(c.encode('hex'))
+            line.append(c)
             if c == '\r':
-                print(line)
+                packet = Packet.create(line)
+                print(packet)
                 line = []
                 break
 finally:
